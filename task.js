2d30e7606ed544a054abcd1eb7cc71a01e3981f2
Leena <leenaheshamm@gmail.com>
2024-08-26 16:00:55 +0300

    used isNaN in if condition then switch in the else to check the result

---
+++ task.js
@@ -0,0 +1,27 @@
+const score = prompt("Enter Score:");
+if (isNaN(score)) {
+  console.log("not a number");
+} else {
+  switch (true) {
+    case score === 100:
+      console.log("Perfect Score");
+      break;
+    case score > 100 || score < 0:
+      console.log("Invalid Score");
+      break;
+    case score >= 85:
+      console.log("You got an A");
+      break;
+    case score >= 75:
+      console.log("You got a B");
+      break;
+    case score >= 65:
+      console.log("You got a C");
+      break;
+    case score >= 50:
+      console.log("You got a D");
+      break;
+    default:
+      console.log("You got an F");
+  }
+}
